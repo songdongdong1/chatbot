141230789ced03e7b3ea9ff8b4ed1aa25390b383
songdongdong1 <songjoy2012@gmail.com>
2025-08-08 14:17:40 -0700

    init

---
+++ chatbot_server/chatbot/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+# chatbot/views.py
+import os
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from . google.init import google_api
+import json
+
+ai_client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+
+@csrf_exempt
+def chatbot_response(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user_message = data['message']
+
+        messages = [
+            {"role": "user", "content": user_message},
+        ]
+        chat_completion = ai_client.chat.completions.create(
+            messages=messages,
+            model="gpt-3.5-turbo",
+        )
+        bot_response = chat_completion.choices[0].message.content
+
+        return JsonResponse({'message': bot_response})
+    else:
+        return JsonResponse({'error': 'Invalid request'}, status=400)
