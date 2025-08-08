132358502f750fa7fdc4e0ab84ea3ab015c2997e
songdongdong1 <songjoy2012@gmail.com>
2025-08-08 13:39:02 -0700

    init

--- chatbot_frontend/src/App.js
+++ chatbot_frontend/src/App.js
@@ -1,5 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+// src/App.js
+import React from 'react';
+import Chatbot from './Chatbot';
+/*
+function App() {
+  return (
+    <div className="App">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;*/
 
 function App() {
   return (
